d3fc03cfe80d26bf0d623a5f7ea9b0a277f8f918
Kurt <kurt.debruijn@gmail.com>
2020-12-14 10:49:21 -0800

    remove comments

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -15,7 +15,7 @@ public class Rook extends ChessPiece {
      */
     public boolean moveTo(Position targetPosition) {
         if (!isValidMove(targetPosition)) return false;
-        //If we get here - is is a valid move. Physically move the piece and answer true.
+
         getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
@@ -28,7 +28,6 @@ public class Rook extends ChessPiece {
     }
 
     private boolean isTargetSameColour(Position targetPosition) {
-        //Next - Check to make sure that if the target square is occupied it is not the same color
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
         if (targetPiece != null) {
             return targetPiece.getColor() == getColor();
@@ -38,18 +37,12 @@ public class Rook extends ChessPiece {
 
 
     private boolean isHorizontalPathNotClear(Position targetPosition) {
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
-        // the increment might be positive or negative.
         if (targetPosition.y != getPosition().y)
             return false;
         return isPositionsClear(getVerticalPositions(targetPosition));
     }
 
     private boolean isVerticalPathNotClear(Position targetPosition) {
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
         if (targetPosition.x != getPosition().x)
             return false;
         return isPositionsClear(getHorizontalPositions(targetPosition));
@@ -94,7 +87,6 @@ public class Rook extends ChessPiece {
     }
 
     private boolean isDiagonalMove(Position targetPosition) {
-        //if it is not the same x or y coordinate it is not a rooks valid move at all
         return targetPosition.x != getPosition().x && targetPosition.y != getPosition().y;
     }
 
