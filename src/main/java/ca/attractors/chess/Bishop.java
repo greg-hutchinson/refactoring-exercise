9163f71bfe6d66187419d0b6067c28ba4aade6e7
Kurt <kurt.debruijn@gmail.com>
2020-12-14 11:31:10 -0800

    add initial bishop tests

--- src/main/java/ca/attractors/chess/Bishop.java
+++ src/main/java/ca/attractors/chess/Bishop.java
@@ -2,8 +2,20 @@ package ca.attractors.chess;
 
 public class Bishop extends ChessPiece {
 
-    protected Bishop(Chessboard chessboard) {
-        super(chessboard, PieceColor.White);
+    protected Bishop(Chessboard chessboard, PieceColor colour) {
+        super(chessboard, colour);
     }
 
+    public boolean moveTo(Position position){
+        if (!isValidMove(position)) {
+            return false;
+        }
+
+        getChessboard().movePieceTo(this, position);
+        return true;
+    }
+
+    public boolean isValidMove(Position position) {
+        return true;//TODO
+    }
 }
--- src/test/java/ca/attractors/chess/BishopTest.java
+++ src/test/java/ca/attractors/chess/BishopTest.java
@@ -4,6 +4,8 @@ package ca.attractors.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static ca.attractors.chess.PieceColor.Black;
+import static ca.attractors.chess.PieceColor.White;
 import static ca.attractors.chess.Position.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +16,43 @@ public class BishopTest {
     @BeforeEach
     void initialize() {
         chessboard = new Chessboard();
-        bishop = new Bishop(chessboard);
+        bishop = new Bishop(chessboard, White);
         chessboard.putPieceAt(bishop, D4);
+    }
+
+
+
+    @Test
+    void moveToNonDiagonalSpot() {
+        assertFalse(bishop.moveTo(D6));
+    }
+
+    @Test
+    void moveToOccupiedCellOfSameColor() {
+        Bishop bishop2 = new Bishop(chessboard, White);
+        chessboard.putPieceAt(bishop2, D3);
+        assertFalse(bishop2.moveTo(D3));
+    }
+
+    @Test
+    void moveToOccupiedCellOfDifferentColor() {
+        Bishop bishop2 = new Bishop(chessboard, Black);
+        chessboard.putPieceAt(bishop2, A1);
+        assertTrue(bishop.moveTo(A1));
+        assertSame(bishop.getPosition(), A1);
+        assertNull (chessboard.getPieceAt(D4));
+    }
+
+    @Test
+    void moveDiagonallyWithUnoccupiedCells() {
+        assertTrue(bishop.moveTo(F6));
+        assertSame(bishop.getPosition(), F6);
+    }
+    @Test
+    void moveDiagonallyToCellWithOccupiedCellsInBetween() {
+        Bishop bishop2 = new Bishop(chessboard, Black);
+        chessboard.putPieceAt(bishop2, E5);
+        assertFalse(bishop2.moveTo(F6));
     }
 
 }
