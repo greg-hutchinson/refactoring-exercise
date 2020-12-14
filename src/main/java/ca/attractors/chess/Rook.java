dc954b25ee5d011c8244a52d0e40174b264ae84b
Kurt <kurt.debruijn@gmail.com>
2020-12-14 08:44:05 -0800

    extract isInvalidMove method

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -14,15 +14,22 @@ public class Rook extends ChessPiece {
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
+        if (isInvalidMove(targetPosition)) return false;
+        //If we get here - is is a valid move. Physically move the piece and answer true.
+        getChessboard().movePieceTo(this, targetPosition);
+        return true;
+    }
+
+    private boolean isInvalidMove(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
         if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
-            return false;
+            return true;
         }
         //Next - Check to make sure that if the target square is occupied it is not the same color
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
         if (targetPiece != null) {
             if (targetPiece.getColor() == getColor())
-                return false;
+                return true;
         }
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
@@ -41,7 +48,7 @@ public class Rook extends ChessPiece {
             }
             for (Position position: positions) {
                 if (getChessboard().getPieceAt(position) != null) {
-                    return false;
+                    return true;
                 }
             }
         }
@@ -62,14 +69,12 @@ public class Rook extends ChessPiece {
             }
             for (Position position: positions) {
                 if (getChessboard().getPieceAt(position) != null) {
-                    return false;
+                    return true;
                 }
             }
         }
-        //If we get here - is is a valid move. Physically move the piece and answer true.
-        getChessboard().movePieceTo(this, targetPosition);
-        return true;
+        return false;
     }
-    
+
 
 }
