17de634042dec2f021eeed0a888163496cd65804
Derek Frombach <derek.frombach@fcc-fac.ca>
2023-02-13 13:08:10 -0600

    Tests are Broken for Refactor First Pass

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -15,14 +15,12 @@ public class Rook extends ChessPiece {
      */
     public boolean moveTo(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
+        if (!checkPosition(targetPosition)){
             return false;
         }
         //Next - Check to make sure that if the target square is occupied it is not the same color
-        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        if (targetPiece != null) {
-            if (targetPiece.getColor() == getColor())
-                return false;
+        if (!checkColor(targetPosition)){
+            return false;
         }
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
@@ -69,6 +67,17 @@ public class Rook extends ChessPiece {
         //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
+    }
+
+    private boolean checkColor(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        return targetPiece != null
+                && targetPiece.getColor() == getColor();
+    }
+
+    private boolean checkPosition(Position targetPosition) {
+        return targetPosition.x != getPosition().x
+                && targetPosition.y != getPosition().y;
     }
 
 }
