26b5a946ce0ebff9d9454ebdc8d2c2ebd819a822
Derek Frombach <derek.frombach@fcc-fac.ca>
2023-02-13 13:53:16 -0600

    Renames

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -14,27 +14,18 @@ public class Rook extends ChessPiece {
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
-        //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (!isTargetPositionValid(targetPosition)){
+        if (isTargetPositionInvalid(targetPosition)
+            || isColorInvalid(targetPosition)
+            || isPathBlocked(targetPosition)){
             return false;
         }
-        //Next - Check to make sure that if the target square is occupied it is not the same color
-        if (!isColorValid(targetPosition)){
-            return false;
-        }
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
-        if (isPathEmpty(targetPosition)) {
-            return false;
-        }
-        //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
 
-    private boolean isPathEmpty(Position targetPosition) {
-        int start,end;
+    private boolean isPathBlocked(Position targetPosition) {
+        int start;
+        int end;
 
         if (targetPosition.y == getPosition().y) {
             start = getPosition().getXOffset();
@@ -46,10 +37,12 @@ public class Rook extends ChessPiece {
         }
 
         int increment = 0;
-        if (start > end)
+        if (start > end) {
             increment = -1;
-        else
+        }
+        else {
             increment = 1;
+        }
 
         List<Position> positions = new ArrayList<>();
 
@@ -71,14 +64,14 @@ public class Rook extends ChessPiece {
         return false;
     }
 
-    private boolean isColorValid(Position targetPosition) {
+    private boolean isColorInvalid(Position targetPosition) {
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        return !(targetPiece != null
-                && targetPiece.getColor() == getColor());
+        return targetPiece != null
+                && targetPiece.getColor() == getColor();
     }
 
-    private boolean isTargetPositionValid(Position targetPosition) {
-        return !(targetPosition.x != getPosition().x
-                && targetPosition.y != getPosition().y);
+    private boolean isTargetPositionInvalid(Position targetPosition) {
+        return targetPosition.x != getPosition().x
+                && targetPosition.y != getPosition().y;
     }
 }
