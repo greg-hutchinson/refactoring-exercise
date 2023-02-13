7aef23b909203ec516e6e52c3fa6b82fede934f3
Greg Hutchinson <the.greg.hutchinson@gmail.com>
2023-02-12 20:07:53 -0600

    Added Queen

---
+++ src/main/java/ca/attractors/chess/Queen.java
@@ -0,0 +1,7 @@
+package ca.attractors.chess;
+
+public class Queen extends ChessPiece {
+    public Queen(PieceColor color, Board board, Position position) {
+        super(color, board, position);
+    }
+}
---
+++ src/test/java/ca/attractors/chess/QueenTest.java
@@ -0,0 +1,46 @@
+package ca.attractors.chess;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static ca.attractors.chess.PieceColor.Black;
+import static ca.attractors.chess.PieceColor.White;
+import static ca.attractors.chess.Position.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueenTest {
+    private Board board;
+
+    @BeforeEach
+    void initialize() {
+        board = new Board();
+    }
+
+
+
+    @Test
+    void moveToNonHorizontalOrNonVerticalOrNonSpot() {
+        Queen queen = new Queen(White, board, A1);
+//        assertFalse(queen.moveTo(B3));
+    }
+
+    @Test
+    void moveHorizontally() {
+        Queen queen = new Queen(White, board, A1);
+//        assertTrue(queen.moveTo(C1));
+    }
+    @Test
+    void moveVertically() {
+        Queen queen = new Queen(White, board, A1);
+//        assertTrue(queen.moveTo(A4));
+    }
+
+    @Test
+    void moveDiagonally() {
+        Queen queen = new Queen(White, board, A1);
+//        assertTrue(queen.moveTo(C3));
+    }
+
+
+}
