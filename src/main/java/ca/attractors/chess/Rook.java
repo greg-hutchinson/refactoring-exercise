6600d4b3415f3a8a43b5441d00c99aff854bd491
Vlad Stepanyuk <vlad.stepanyuk@fcc-fac.ca>
2023-02-13 15:20:41 -0500

    Refactoring Rook move

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -1,74 +1,91 @@
 package ca.attractors.chess;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Rook extends ChessPiece {
     protected Rook(PieceColor color, Board board, Position position) {
         super(color, board, position);
     }
 
     /**
-     *
      * @param targetPosition - the position that we would like to move to
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
-        //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
+        if (!isMoveValid(targetPosition)) {
             return false;
-        }
-        //Next - Check to make sure that if the target square is occupied it is not the same color
-        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        if (targetPiece != null) {
-            if (targetPiece.getColor() == getColor())
-                return false;
-        }
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
-        if (targetPosition.x == getPosition().x) {
-            int start = getPosition().getYOffset();
-            int end = targetPosition.getYOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int y = start+increment; y != end; y = y + increment) {
-                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
-            }
-        }
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
-        // the increment might be positive or negative.
-        if (targetPosition.y == getPosition().y) {
-            int start = getPosition().getXOffset();
-            int end = targetPosition.getXOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int x = start+increment; x != end; x = x + increment) {
-                positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
-            }
         }
         //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
+    }
+
+    private boolean isMoveValid(Position targetPosition) {
+        if (!isMoveValidForPiece(targetPosition)
+                || isSquareOccupiedWithSameColor(targetPosition)) {
+            return false;
+        }
+        // VS: It is a vertical move!!!
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
+        // the increment might be positive or negative.
+        if (isVerticalMove(targetPosition) &&
+                !isVerticalMoveValid(targetPosition))  {
+            return false;
+        }
+        return !isHorizontalMove(targetPosition)
+                || isHorizontalMoveValid(targetPosition);
+    }
+
+    private boolean isMoveValidForPiece(Position targetPosition) {
+        return isVerticalMove(targetPosition) || isHorizontalMove(targetPosition);
+    }
+
+    private boolean isVerticalMove(Position targetPosition) {
+        return targetPosition.x == getPosition().x;
+    }
+
+    private boolean isHorizontalMove(Position targetPosition) {
+        return targetPosition.y == getPosition().y;
+    }
+
+    private boolean isSquareOccupiedWithSameColor(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        if (targetPiece == null) {
+            return false;
+        }
+        return targetPiece.getColor() == getColor();
+    }
+
+    private boolean isVerticalMoveValid(Position targetPosition) {
+        int start = getPosition().getYOffset();
+        int end = targetPosition.getYOffset();
+        int increment = getIncrement(start, end);
+        for (int y = start + increment; y != end; y = y + increment) {
+            Position position = Position.getPositionFor(targetPosition.getXOffset(), y);
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isHorizontalMoveValid(Position targetPosition) {
+        int start = getPosition().getXOffset();
+        int end = targetPosition.getXOffset();
+        int increment = getIncrement(start, end);
+        for (int x = start + increment; x != end; x = x + increment) {
+            Position position = Position.getPositionFor(x, targetPosition.getYOffset());
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int getIncrement(int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        return 1;
     }
 
 }
