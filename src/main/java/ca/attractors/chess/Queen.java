7262c35e89496f9f2946ed36a807f06fff8f7b8c
Vlad Stepanyuk <vlad.stepanyuk@fcc-fac.ca>
2023-02-13 16:00:40 -0500

    refactoring Queen

--- src/main/java/ca/attractors/chess/Queen.java
+++ src/main/java/ca/attractors/chess/Queen.java
@@ -7,6 +7,75 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean isMoveValid(Position targetPosition) {
-        return false;
+        if (!isMoveValidForPiece(targetPosition) ||
+                isPositionOccupiedWithSameColor(targetPosition)) {
+            return false;
+        }
+        if (targetPosition.isHorizontalMove(getPosition())) {
+            return isHorizontalMoveValid(targetPosition);
+        }
+        if (targetPosition.isVerticalMove(getPosition())) {
+            return isVerticalMoveValid(targetPosition);
+        }
+        if (targetPosition.isDiagonalMove(getPosition())) {
+            return isDiagonalMoveValid(targetPosition);
+        }
+        return true;
     }
+
+    private boolean isMoveValidForPiece(Position targetPosition) {
+        return targetPosition.isHorizontalMove(getPosition())
+                || targetPosition.isVerticalMove(getPosition()) || (targetPosition.isDiagonalMove(getPosition()));
+    }
+
+    private boolean isDiagonalMoveValid(Position targetPosition) {
+        int startY = getPosition().getYOffset();
+        int endY = targetPosition.getYOffset();
+        int startX = getPosition().getYOffset();
+        int endX = targetPosition.getYOffset();
+        int incrementX = getIncrement(startX, endX);
+        int incrementY = getIncrement(startY, endY);
+        for (int x = startY + incrementY, y = startY + incrementY; (x != endX) || (y != endY); x += incrementX, y += incrementY) {
+            Position position = Position.getPositionFor(x, y);
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isVerticalMoveValid(Position targetPosition) {
+        int start = getPosition().getYOffset();
+        int end = targetPosition.getYOffset();
+        int increment = getIncrement(start, end);
+        for (int y = start + increment; y != end; y += increment) {
+            Position position = Position.getPositionFor(targetPosition.getXOffset(), y);
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isHorizontalMoveValid(Position targetPosition) {
+        int start = getPosition().getXOffset();
+        int end = targetPosition.getXOffset();
+        int increment = getIncrement(start, end);
+        for (int x = start + increment; x != end; x += increment) {
+            Position position = Position.getPositionFor(x, targetPosition.getYOffset());
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int getIncrement(int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        return 1;
+    }
+
+
 }
--- src/test/java/ca/attractors/chess/QueenTest.java
+++ src/test/java/ca/attractors/chess/QueenTest.java
@@ -4,10 +4,14 @@ package ca.attractors.chess;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static ca.attractors.chess.PieceColor.Black;
 import static ca.attractors.chess.PieceColor.White;
-import static ca.attractors.chess.Position.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static ca.attractors.chess.Position.A1;
+import static ca.attractors.chess.Position.A4;
+import static ca.attractors.chess.Position.B3;
+import static ca.attractors.chess.Position.C1;
+import static ca.attractors.chess.Position.C3;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QueenTest {
     private Board board;
@@ -21,24 +25,24 @@ public class QueenTest {
     @Test
     void moveHorizontally() {
         Queen queen = new Queen(White, board, A1);
-//        assertTrue(queen.moveTo(C1));
+        assertTrue(queen.moveTo(C1));
     }
     @Test
     void moveVertically() {
         Queen queen = new Queen(White, board, A1);
-//        assertTrue(queen.moveTo(A4));
+        assertTrue(queen.moveTo(A4));
     }
 
     @Test
     void moveDiagonally() {
         Queen queen = new Queen(White, board, A1);
-//        assertTrue(queen.moveTo(C3));
+        assertTrue(queen.moveTo(C3));
     }
 
     @Test
     void moveToNonHorizontalOrNonVerticalOrNonSpot() {
         Queen queen = new Queen(White, board, A1);
-//        assertFalse(queen.moveTo(B3));
+        assertFalse(queen.moveTo(B3));
     }
 
 }
