29ccd10a0f11e0301cdb8e0d6907d230037db85c
Matthew Ruttan <ruttanm@fcc.ca>
2023-03-06 14:23:57 -0500

    using inner class, finish logic for isInvalidMove

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -1,7 +1,9 @@
 package ca.attractors.chess;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Rook extends ChessPiece {
     protected Rook(PieceColor color, Board board, Position position) {
@@ -14,61 +16,30 @@ public class Rook extends ChessPiece {
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
-        //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) { //don't need to change
+        if (isDiagonalMove(targetPosition))
             return false;
-        }
-        //Next - Check to make sure that if the target square is occupied it is not the same color
-        if (isSquareOccupiedBySameColor(targetPosition)) return false;
 
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
+        if (isSquareOccupiedBySameColor(targetPosition))
+            return false;
 
         if (targetPosition.x == getPosition().x) {
-            if (isInvalidHorizontalMove(targetPosition)) return false;
+            PiecePath horizontalPath = new PiecePath(getPosition().getYOffset(), targetPosition.getYOffset(), "HORIZONTAL");
+            if (isInvalidMove(targetPosition, horizontalPath))
+                return false;
         }
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
-        // the increment might be positive or negative.
+
         if (targetPosition.y == getPosition().y) {
-            if (isInvalidVerticalMove(targetPosition)) return false;
+            PiecePath verticalPath = new PiecePath(getPosition().getXOffset(), targetPosition.getXOffset(), "VERTICAL");
+            if (isInvalidMove(targetPosition, verticalPath))
+                return false;
         }
         //If we get here - it is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
 
-    private boolean isInvalidVerticalMove(Position targetPosition) {
-        int start = getPosition().getXOffset();
-        int end = targetPosition.getXOffset();
-        int increment = targetPosition.getIncrement(start, end);
-        List<Position> positions = new ArrayList<>();
-        for (int x = start+increment; x != end; x = x + increment) {
-            positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
-        }
-        for (Position position: positions) {
-            if (isSuccessfulMove(position)) return true;
-        }
-        return false;
-    }
-
-    private boolean isInvalidHorizontalMove(Position targetPosition) {
-        int start = getPosition().getYOffset();
-        int end = targetPosition.getYOffset();
-        int increment = targetPosition.getIncrement(start, end);
-        List<Position> positions = new ArrayList<>();
-        for (int y = start+increment; y != end; y = y + increment) {
-            positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
-        }
-        for (Position position: positions) {
-            if (isSuccessfulMove(position)) return true;
-        }
-        return false;
-    }
-
-    private boolean isSuccessfulMove(Position position) {
-        return getChessboard().getPieceAt(position) != null;
+    private boolean isDiagonalMove(Position targetPosition) {
+        return targetPosition.x != getPosition().x && targetPosition.y != getPosition().y;
     }
 
     private boolean isSquareOccupiedBySameColor(Position targetPosition) {
@@ -76,5 +47,62 @@ public class Rook extends ChessPiece {
         if (targetPiece != null)
             return targetPiece.getColor() == getColor();
         return false;
+    }
+
+    private boolean isInvalidMove(Position targetPosition, PiecePath path) {
+        int increment = targetPosition.getIncrement(path.getStart(), path.getEnd());
+        List<Position> positions = new ArrayList<>();
+        if(Objects.equals(path.getDirection(), "HORIZONTAL"))
+            positions = getHorizontalPositions(targetPosition, path, increment);
+        else if(Objects.equals(path.getDirection(), "VERTICAL"))
+            positions = getVerticalPositions(targetPosition, path, increment);
+        for (Position position: positions) {
+            if (isSuccessfulMove(position)) return true;
+        }
+        return false;
+    }
+
+    private static List<Position> getVerticalPositions(Position targetPosition, PiecePath path, int increment) {
+        List<Position> positions = new ArrayList<>();
+        for (int x = path.getStart() + increment; x != path.getEnd(); x = x + increment) {
+            positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
+        }
+        return positions;
+    }
+
+    private static List<Position> getHorizontalPositions(Position targetPosition, PiecePath path, int increment) {
+        List<Position> positions = new ArrayList<>();
+        for (int y = path.getStart() + increment; y != path.getEnd(); y = y + increment) {
+            positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+        }
+        return positions;
+    }
+
+    private boolean isSuccessfulMove(Position position) {
+        return getChessboard().getPieceAt(position) != null;
+    }
+
+    static class PiecePath {
+        private final int start;
+        private final int end;
+        private final String direction;
+
+        PiecePath(int start, int end, String direction) {
+            this.start = start;
+            this.end = end;
+            this.direction = direction;
+        }
+
+        public int getStart() {
+            return this.start;
+        }
+
+        public int getEnd() {
+            return this.end;
+        }
+
+        public String getDirection() {
+            return this.direction;
+        }
     }
 }
