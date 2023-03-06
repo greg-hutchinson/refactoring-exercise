180e45c58e8489ecf858f7b420b0cb53343e1056
Dillon Forsberg <forsbed1@fcc.ca>
2023-03-06 11:53:29 -0600

    Extract isPathImpeded method

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -16,54 +16,47 @@ public class Rook extends ChessPiece {
     public boolean moveTo(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
         if (isTargetPositionCoordsInvalid(targetPosition)) return false;
-
+        
         //Next - Check to make sure that if the target square is occupied it is not the same color
         if (isOccupiedBySameColour(targetPosition)) return false;
-
+        
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
-        if (targetPosition.x == getPosition().x) {
-            int start = getPosition().getYOffset();
-            int end = targetPosition.getYOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int y = start+increment; y != end; y = y + increment) {
-                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
-            }
-        }
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
         // the increment might be positive or negative.
-        if (targetPosition.y == getPosition().y) {
-            int start = getPosition().getXOffset();
-            int end = targetPosition.getXOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int x = start+increment; x != end; x = x + increment) {
-                positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
-            }
-        }
+        if (isPathImpeded(targetPosition)) return false;
+
         //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
+        return true;
+    }
+
+    private boolean isPathImpeded(Position targetPosition) {
+        // Assume we are moving in Y direction by default
+        int start = getPosition().getYOffset();
+        int end = targetPosition.getYOffset();
+
+        // If Y position hasn't changed, then we are moving in the X direction
+        if (targetPosition.y == getPosition().y) {
+            start = getPosition().getXOffset();
+            end = targetPosition.getXOffset();
+        }
+
+        int increment = 0;
+        if (start > end)
+            increment = -1;
+        else
+            increment = 1;
+        List<Position> positions = new ArrayList<>();
+        for (int y = start+increment; y != end; y = y + increment) {
+            positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+        }
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+
         return true;
     }
 
