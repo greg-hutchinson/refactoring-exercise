9c361910f46dc69fefb70c56b8aa0de446d02bcd
Dillon Forsberg <forsbed1@fcc.ca>
2023-03-06 13:24:00 -0600

    Extract isValidMove method

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -14,20 +14,27 @@ public class Rook extends ChessPiece {
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
+        if (isValidMove(targetPosition)) {
+            //If we get here - is is a valid move. Physically move the piece and answer true.
+            getChessboard().movePieceTo(this, targetPosition);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isValidMove(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
         if (!isTargetPositionCoordsValid(targetPosition)) return false;
-        
+
         //Next - Check to make sure that if the target square is occupied it is not the same color
         if (isOccupiedBySameColour(targetPosition)) return false;
-        
+
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
         // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
         // the increment might be positive or negative.
         if (isPathImpeded(targetPosition)) return false;
 
-        //If we get here - is is a valid move. Physically move the piece and answer true.
-        getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
 
