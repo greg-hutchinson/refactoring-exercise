1e99c3b1339c7f64edb22be287e5b3bb2952a187
Dillon Forsberg <forsbed1@fcc.ca>
2023-03-06 11:37:44 -0600

    Extract isOccupiedBySameColour method

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -19,11 +19,8 @@ public class Rook extends ChessPiece {
             return false;
         }
         //Next - Check to make sure that if the target square is occupied it is not the same color
-        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        if (targetPiece != null) {
-            if (targetPiece.getColor() == getColor())
-                return false;
-        }
+        if (isOccupiedBySameColour(targetPosition)) return false;
+
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
         // the increment might be positive or negative.
@@ -69,6 +66,15 @@ public class Rook extends ChessPiece {
         //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
+    }
+
+    private boolean isOccupiedBySameColour(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        if (targetPiece != null) {
+            if (targetPiece.getColor() == getColor())
+                return true;
+        }
+        return false;
     }
 
 }
